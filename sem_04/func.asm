aebe9485e867aa99ec54d4204ef6999bf2993ca8
Evgeniy Kuznetsov <linuxoved@yandex.ru>
2021-03-26 18:43:22 +0300

    26.03.2021 Added sem_04

---
+++ sem_04/func.asm
@@ -0,0 +1,31 @@
+
+; fasm demonstration of writing simple ELF executable
+
+format ELF executable 3
+entry start
+
+segment readable executable
+
+start:
+
+    call show_message
+
+    ; now exit 
+   	mov	eax,1           ; sys_exit
+	xor	ebx,ebx         ; exit code == 0
+	int	0x80            ; Вызов прерывания
+
+
+show_message:
+
+	mov	eax,4           ; sys_write - код 4
+	mov	ebx,1           ; stdout   mycommand 2>&1    0 - STDIN, 1 - STDOUT, 2 - STDERR
+	mov	ecx,msg         ; Адрес строки, т.е 1-го байта 
+	mov	edx,msg_size    ; Размер строки
+	int	0x80            ; Вызов прерывания
+    ret                 ; возврат в точку вызова
+   
+segment readable writeable
+
+msg db 'Hello world!',0xA
+msg_size = $-msg
